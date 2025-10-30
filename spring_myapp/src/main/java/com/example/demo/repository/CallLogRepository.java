6397850ed705a39e0c3c67db4b14752e358e9e3a
yuki <ym7yu29myle@gmail.com>
2025-10-30 21:12:50 +0900

    コメントアウトの追加、整理

--- spring_myapp/src/main/java/com/example/demo/repository/CallLogRepository.java
+++ spring_myapp/src/main/java/com/example/demo/repository/CallLogRepository.java
@@ -1,12 +1,12 @@
 package com.example.demo.repository;
 
-import java.util.List;
+import java.util.List;//List型を使う
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;//JpaRepositoryをインポートし基本的なデータベース操作を自動的に利用可
 
-import com.example.demo.model.CallLog;
+import com.example.demo.model.CallLog;//CallLogクラスをインポート
 
-public interface CallLogRepository extends JpaRepository<CallLog, Long> {
-	// ユーザーIDで通話ログを取得するメソッドを追加
-    List<CallLog> findByUserId(Long userId);
+public interface CallLogRepository extends JpaRepository<CallLog, Long> {//JpaRepositoryで自動的に基本的なDB操作が利用可
+	
+    List<CallLog> findByUserId(Long userId);//ユーザーIDで通話ログを取得する処理を追加
 }
