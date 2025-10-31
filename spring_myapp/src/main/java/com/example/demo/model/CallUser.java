5308c7f67208682ec32c0c51ac1e6269e94ce38d
yuki <ym7yu29myle@gmail.com>
2025-10-31 22:10:22 +0900

    コントローラー、htmlのコードの追加

--- spring_myapp/src/main/java/com/example/demo/model/CallUser.java
+++ spring_myapp/src/main/java/com/example/demo/model/CallUser.java
@@ -1,16 +1,21 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;//@Entityを使う
 import jakarta.persistence.GeneratedValue;//主キーの値を自動生成
 import jakarta.persistence.GenerationType;//自動採番の戦略を指定
 import jakarta.persistence.Id;//主キーを指定
+import jakarta.persistence.Table;
+
 
 @Entity//データベースのテーブルと対応付けるエンティティクラス
+@Table(name = "call_users")
 public class CallUser {
     @Id//テーブルの主キー
     @GeneratedValue(strategy = GenerationType.IDENTITY)//IDを自動採番する設定(MySQLで)
     private Long id;//ID。データベースの主キー
-    private String username;//ユーザー名
+    @Column(name = "username")
+    private String userName;//ユーザー名
     private String password;//パスワード
     private String role;//ユーザーの権限
 
@@ -24,11 +29,11 @@ public class CallUser {
     }
 
     public String getUsername() {//usernameカラムの値を取得
-        return username;
+        return userName;
     }
 
-    public void setUsername(String username) {//usernameカラムに値を設定
-        this.username = username;
+    public void setUsername(String userName) {//usernameカラムに値を設定
+        this.userName = userName;
     }
 
     public String getPassword() {//passwordカラムの値を取得
@@ -45,5 +50,13 @@ public class CallUser {
 
     public void setRole(String role) {//roleカラムに値を設定
         this.role = role;
+    }
+    
+    @Override
+    public String toString() {//このエンティティを文字列にするときの型を定義
+        return "CallLog{id=" + id + 
+               ", userName=" + userName + 
+               ", password='" + password + 
+               ", role='" + role + "'}";
     }
 }
--- spring_myapp/src/main/java/com/example/demo/repository/CallUserRepository.java
+++ spring_myapp/src/main/java/com/example/demo/repository/CallUserRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;//JpaRepositoryをインポートし、SQLを書かずに基本的なCRUD操作（登録・更新・削除・検索）が使える
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.CallUser;//CallUserクラスをインポート
 
+@Repository
 public interface CallUserRepository extends JpaRepository<CallUser, Long> {// CallUserRepositoryの定義
 	// 以下の処理が自動的に利用可能になる
 	// findAll()      → 全件取得
